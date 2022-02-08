d20a26097a109fb67fe992c101c509631d06e0b0
DaniyarZh <den-gak@mail.ru>
2022-02-08 19:20:24 +0400

    create functions

--- dragon_game.py
+++ dragon_game.py
@@ -1,11 +1,28 @@
 
-def main():
+def dragon_create():
+    global health
     health = 100
+    
+def dragon_is_alive():
+    return health > 0
+
+def dragon_get_damage(damage):
+    global health 
+    health -= damage
+    if health < 0:
+        health = 0
+
+def main():
+    dragon_create()
     finish = False
     while not finish:
         print('My health', health, '. Hit me!')
         damage = int(input())
-        health -= damage
+        dragon_get_damage(damage)
+        if not dragon_is_alive():
+            finish = True
+
+    print('YOu win')
 
 
 main()
