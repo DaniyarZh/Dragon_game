4d32ca7012c37fcaf3a7fdcafe9d35f43b8bb72c
DaniyarZh <den-gak@mail.ru>
2022-02-08 17:39:14 +0400

    Прототип игры

---
+++ dragon_game.py
@@ -0,0 +1,12 @@
+
+def main():
+    health = 100
+    finish = False
+    while not finish:
+        print('My health', health, '. Hit me!')
+        damage = int(input())
+        health -= damage
+
+
+main()
+        
